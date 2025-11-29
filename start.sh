c48d690438a8bf0e5ccaff43bb71d8a1afc2087e
Arthur Dos Santos Sousa <102875535+KiritoGamesPlays@users.noreply.github.com>
2025-11-29 14:35:40 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
+#!/bin/bash 
 
 # Diretório correto do Pterodactyl
 export N8N_HOME=/home/container/.n8n
 mkdir -p "$N8N_HOME"
 
-# Caminho default
+# Outros diretórios importantes
 export N8N_USER_FOLDER="$N8N_HOME"
+export N8N_CONFIG_FILES="$N8N_HOME/config"
+
+mkdir -p "$N8N_CONFIG_FILES"
 
 # Porta usada pelo Pterodactyl
 export N8N_PORT={{SERVER_PORT}}
@@ -15,6 +18,8 @@ export N8N_ENCRYPTION_KEY=$(openssl rand -hex 16)
 
 # Logs mais limpos
 export N8N_LOG_LEVEL=debug
+
+cd /home/container
 
 # Iniciar o n8n
 n8n start
